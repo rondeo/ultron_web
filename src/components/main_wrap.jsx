a596bdabd68d7470b163e1516249f94b57373638
etongfu <531202571@qq.com>
2019-05-13 12:35:01 +0800

    content部分

--- src/components/main_wrap.jsx
+++ src/components/main_wrap.jsx
@@ -3,23 +3,27 @@
  * @Version: 
  * @Email: 13583254085@163.com
  * @LastEditors: etongfu
- * @Description: 内容包裹层
+ * @Description: 网页主要内容的一个包裹层
  * @youWant: add you want info here
  * @Date: 2019-05-08 20:55:40
- * @LastEditTime: 2019-05-08 20:59:43
+ * @LastEditTime: 2019-05-13 11:29:35
  */
 import React from 'react'
 import HomeHeader from './home_header';
 
 class MainWrap extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { }
-  }
+
   render() {
     return (
       <main className="app-content-wrap">
-        <HomeHeader name="主页" />
+        {/* 是否渲染header */}
+        {
+          this.props.widthHeader ? <HomeHeader name={this.props.headerName || ""} /> : ''
+        }
+        {/* 主要内容 */}
+        <section className={`app-content ${this.props.widthHeader ? 'app-content-with-header': ''}`} >
+          {this.props.children}
+        </section>
       </main>
     )
   }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -6,14 +6,15 @@
  * @Description: home页面
  * @youWant: add you want info here
  * @Date: 2019-04-30 15:57:50
- * @LastEditTime: 2019-05-08 20:58:57
+ * @LastEditTime: 2019-05-13 11:29:53
  */
 import React from 'react'
 import MainWrap from 'components/main_wrap';
 
-
 export default class Home extends React.Component {
   render () {
-    return <MainWrap />
+    return <MainWrap headerName="主页" widthHeader>
+        开发中...
+    </MainWrap>
   }
 }
